b0eb807f8e3bff5551065347111422ad11eab830
reymanuel010899 <reymanuelferrera78@gmail.com>
2024-02-01 15:04:59 -0400

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## empresa de tecnologias
+
+ventas de computadores y laptos al por mayor y detalles
+
+#### License
+
+MIT
---
+++ guiatechnology/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "empresa de tecnologias",
+			"type": "module",
+			"label": _("empresa de tecnologias")
+		}
+	]
